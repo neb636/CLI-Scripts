93f77a60a48ce53008b770a3ff42ae02bd30f336
Nick Blanchard <nblanchard@thinkoomph.com>
2014-03-29 20:08:07 -0400

    Updated replace script to generate a backup file

--- replace.rb
+++ replace.rb
@@ -3,26 +3,41 @@
 # Simple class to do search and replace within a directory
 class Replace
 
-  def initialize(original, replacement)
+  require 'fileutils'
+
+  def initialize(backup, original, replacement)
+    @backup = backup
     @original = original
     @replacement = replacement
   end
 
   # Loop through every file and perform search and replace
   def write_replace
-
-    # First set the file locations only if the correct extension
     files = Dir.glob('**/**')
 
     files.each do |file_name|
 
+      create_backup
       text = File.read(file_name)
       replace = text.gsub(@original, @replacement)
       File.open(file_name, 'w') { |file| file.puts replace }
       puts "Updating #{file_name}"
     end
   end
+
+  # If answered yes create backup
+  def create_backup
+    if @backup == 'y' || @backup == 'yes'
+      new_file = file_name + '.bak'
+      FileUtils.cp file_name, new_file
+    end
+  end
 end
+
+# Questions asked
+
+puts "\nWould you like to make a backup file?"
+backup = gets.chomp
 
 puts "\nWhat is the original string?"
 original = gets.chomp
@@ -30,5 +45,5 @@ original = gets.chomp
 puts "\nWhat is the replacement string?"
 replacement = gets.chomp
 
-replace = Replace.new(original, replacement)
+replace = Replace.new(backup, original, replacement)
 replace.write_replace
